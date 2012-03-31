04a9259792fc1d921c70f438532f9d2fe7db8dbb
Kurt Kretzer <kurt.kretzer@gmail.com>
2012-03-31 09:06:41 -0500

    making pivot choice a ruby block

--- programming_assignment_2.quick_sort.rb
+++ programming_assignment_2.quick_sort.rb
@@ -1,4 +1,4 @@
-def sort_and_count_comparisons(int_array, pivot, sort_range)
+def sort_and_count_comparisons(int_array, pivot, sort_range, &pivot_chooser)
     #puts "\t" + sort_range.to_s
     comparisons = sort_range.count - 1
     return 0 if comparisons <= 0
@@ -16,16 +16,13 @@ def sort_and_count_comparisons(int_array, pivot, sort_range)
     int_array[pivot] = int_array[i-1]
     int_array[i-1] = tmp
     #puts "recursing on upper half"
-    comparisons += sort_and_count_comparisons(int_array, i, (i..(sort_range.last)))
+    upper_half_sort_range = (i..(sort_range.last))
+    comparisons += sort_and_count_comparisons(int_array, yield(upper_half_sort_range), upper_half_sort_range, &pivot_chooser)
     #puts "recursing on lower half"
-    comparisons += sort_and_count_comparisons(int_array, sort_range.first, ((sort_range.first)..(i-2)))
+    lower_half_sort_range = ((sort_range.first)..(i-2))
+    comparisons += sort_and_count_comparisons(int_array, yield(lower_half_sort_range), lower_half_sort_range, &pivot_chooser)
     comparisons
 end
-
-def choose_pivot_always_first(sort_range)
-    sort_range.first
-end
-
 
 file_array = File.open(ARGV[0], 'r') do |file|
     file.readlines.map {|line| line.to_i}
@@ -33,7 +30,9 @@ end
 
 init_sort_range = (0..(file_array.length - 1))
 
-puts sort_and_count_comparisons(file_array, choose_pivot_always_first(init_sort_range), init_sort_range)
+puts sort_and_count_comparisons(file_array, init_sort_range.first, init_sort_range) {|sort_range| sort_range.first}
+
+#puts sort_and_count_comparisons(file_array, init_sort_range.last, init_sort_range)
 
 # write the sorted list to a file to double check correctness
 File.open('/tmp/quicksorted.txt', 'w') {|file| file_array.each {|i| file.puts i}}
