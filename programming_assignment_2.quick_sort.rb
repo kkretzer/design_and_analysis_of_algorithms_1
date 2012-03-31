bbd3795a0493be0eb137657c820d32896dde91d8
Kurt Kretzer <kurt.kretzer@gmail.com>
2012-03-31 12:39:15 -0500

    adding part 2 of 2nd assignment

--- programming_assignment_2.quick_sort.rb
+++ programming_assignment_2.quick_sort.rb
@@ -1,26 +1,25 @@
 def sort_and_count_comparisons(int_array, pivot, sort_range, &pivot_chooser)
-    #puts "\t" + sort_range.to_s
     comparisons = sort_range.count - 1
     return 0 if comparisons <= 0
     i = sort_range.first + 1
     search_range = (i..(sort_range.last))
     search_range.each do |j|
         if int_array[j] < int_array[pivot]
-            tmp = int_array[i]
-            int_array[i] = int_array[j]
-            int_array[j] = tmp
+            int_array[i], int_array[j] = int_array[j], int_array[i]
             i += 1
         end
     end
-    tmp = int_array[pivot]
-    int_array[pivot] = int_array[i-1]
-    int_array[i-1] = tmp
-    #puts "recursing on upper half"
-    upper_half_sort_range = (i..(sort_range.last))
-    comparisons += sort_and_count_comparisons(int_array, yield(upper_half_sort_range), upper_half_sort_range, &pivot_chooser)
-    #puts "recursing on lower half"
-    lower_half_sort_range = ((sort_range.first)..(i-2))
-    comparisons += sort_and_count_comparisons(int_array, yield(lower_half_sort_range), lower_half_sort_range, &pivot_chooser)
+    int_array[pivot], int_array[i-1] = int_array[i-1], int_array[pivot]
+    unless i > sort_range.last
+        upper_half_sort_range = (i..(sort_range.last))
+        upper_pivot = yield(upper_half_sort_range, int_array)
+        comparisons += sort_and_count_comparisons(int_array, upper_pivot, upper_half_sort_range, &pivot_chooser)
+    end
+    unless sort_range.first > i-2
+        lower_half_sort_range = ((sort_range.first)..(i-2))
+        lower_pivot = yield(lower_half_sort_range, int_array)
+        comparisons += sort_and_count_comparisons(int_array, lower_pivot, lower_half_sort_range, &pivot_chooser)
+    end
     comparisons
 end
 
@@ -30,9 +29,23 @@ end
 
 init_sort_range = (0..(file_array.length - 1))
 
-puts sort_and_count_comparisons(file_array, init_sort_range.first, init_sort_range) {|sort_range| sort_range.first}
+# always pick first element of subarray to be partitioned
+#comps = sort_and_count_comparisons(file_array, init_sort_range.first, init_sort_range) do |sort_range, ary|
+#    sort_range.first
+#end
 
-#puts sort_and_count_comparisons(file_array, init_sort_range.last, init_sort_range)
+
+# always pick the last element of subarray to be partitioned
+file_array[0], file_array[file_array.length - 1] = file_array[file_array.length - 1], file_array[0]
+comps = sort_and_count_comparisons(file_array, init_sort_range.first, init_sort_range) do |sort_range, ary|
+    ary[sort_range.first], ary[sort_range.last] = ary[sort_range.last], ary[sort_range.first]
+    sort_range.first
+end
+
+
+puts comps
 
 # write the sorted list to a file to double check correctness
 File.open('/tmp/quicksorted.txt', 'w') {|file| file_array.each {|i| file.puts i}}
+
+
