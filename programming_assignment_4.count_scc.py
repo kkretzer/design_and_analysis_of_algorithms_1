2e469f8e721187998f71509319c401056f2be8b6
Kurt Kretzer <kurt.kretzer@gmail.com>
2012-04-21 11:55:27 -0500

    adding some expected output checking for #4

--- programming_assignment_4.count_scc.py
+++ programming_assignment_4.count_scc.py
@@ -1,5 +1,4 @@
-import sys
-import time
+import sys, time, os
 from string import *
 from collections import defaultdict
 
@@ -9,8 +8,8 @@ sys.setrecursionlimit(1000000)
 def log(msg):
     print time.strftime("%H:%M:%S"), msg
 
-def read_graph_file():
-    with open(sys.argv[1], 'r') as file:
+def read_graph_file(input_file):
+    with open(input_file, 'r') as file:
         forward_adjacency_lists = defaultdict(list)
         reversed_adjacency_lists = defaultdict(list)
         vertices = set([])
@@ -67,10 +66,35 @@ def strongly_connected_components(forward_graph, reverse_graph, vertices):
             dfs(forward_graph_relabeled, i)
     return leader
 
+if len(sys.argv) == 1:
+    input_file = os.path.join(os.path.dirname(__file__), 'SCC.txt')
+else:
+    input_file = sys.argv[1]
+
+# some cheap test setup...
+expected_biggest_5 = None
+if os.path.basename(input_file) == 'test_scc.txt':
+    expected_biggest_5 = [3,3,3]
+elif os.path.basename(input_file) == 'test_scc2.txt':
+    expected_biggest_5 = [7,3]
+elif os.path.basename(input_file) == 'SCC.txt':
+    expected_biggest_5 = [434821, 968, 459, 313, 211]
+
 log("reading in file...")
-input = read_graph_file()
+input = read_graph_file(input_file)
 log("done reading in file")
+
+log("computing strongly connected components...")
 sccs = strongly_connected_components(input[0], input[1], input[2])
+log("done computing strongly connected components")
     
+# only expecting strongly_connected_components to return the SCC's, not in any particular order
+# so rank them by size here
 scc_counts = sorted(sccs.values(), reverse=True)
-print scc_counts[:5]
+
+computed_biggest_5 = scc_counts[:5]
+log("biggest 5 strongly connected components:")
+print computed_biggest_5
+if expected_biggest_5 != None:
+    test_status = ("FAILED", "PASSED")[expected_biggest_5 == computed_biggest_5]
+    log(test_status + " test")
