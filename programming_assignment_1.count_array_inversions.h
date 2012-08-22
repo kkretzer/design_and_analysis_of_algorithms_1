17df8a6f050872d7ae8da1b11c89045443cd7ba3
Kurt Kretzer <kurt.kretzer@gmail.com>
2012-08-22 06:52:28 -0500

    changed mergesort to use memcpy instead of tons of for loops

--- programming_assignment_1.count_array_inversions.h
+++ programming_assignment_1.count_array_inversions.h
@@ -20,55 +20,41 @@ size_t mymergesort(void *input_array, size_t num_elem, size_t elem_width, int (*
 
 static size_t merge(char *to_merge, size_t l_num_elem, size_t r_num_elem, size_t elem_width, int (*compar)(const void *, const void *))
 {
-    char *l_iter, *r_iter, *l_end, *r_end, *temp, *temp_holder;
+    char *l_iter, *r_iter, *l_end, *r_end, *merged;
     size_t inversions;
-    int compar_result, i;
+    int compar_result;
     l_iter = to_merge;
     r_iter = l_end = to_merge + (l_num_elem * elem_width);
     r_end = to_merge + (l_num_elem + r_num_elem)*elem_width;
     inversions = 0;
-    temp = (char *) malloc((l_num_elem + r_num_elem) * elem_width);
-    temp_holder = temp;
+    merged = (char *) malloc((l_num_elem + r_num_elem) * elem_width);
 
     while (l_iter < l_end && r_iter < r_end) {
         compar_result = (*compar)(l_iter, r_iter);
         if (compar_result < 0) {
-            for (i=0; i<elem_width; ++i) {
-                *temp = *l_iter;
-                ++temp;
-                ++l_iter;
-            }
+            memcpy(merged, l_iter, elem_width);
+            merged += elem_width;
+            l_iter += elem_width;
         } else if (compar_result > 0) {
-            for (i=0; i<elem_width; ++i) {
-                *temp = *r_iter;
-                ++temp;
-                ++r_iter;
-            }
+            memcpy(merged, r_iter, elem_width);
+            merged += elem_width;
+            r_iter += elem_width;
             inversions += ((l_end - l_iter)/elem_width);
         } else {
-            for (i=0; i<elem_width; ++i) {
-                *temp = *l_iter;
-                ++temp;
-                ++l_iter;
-            }
-            for (i=0; i<elem_width; ++i) {
-                *temp = *r_iter;
-                ++temp;
-                ++r_iter;
-            }
+            memcpy(merged, l_iter, elem_width);
+            merged += elem_width;
+            l_iter += elem_width;
+            memcpy(merged, r_iter, elem_width);
+            merged += elem_width;
+            r_iter += elem_width;
         }
     }
-    while (l_iter < l_end) {
-        *temp = *l_iter;
-        ++temp;
-        ++l_iter;
-    }
-    while (r_iter < r_end) {
-        *temp = *r_iter;
-        ++temp;
-        ++r_iter;
-    }
-    memcpy(to_merge, temp_holder, (l_num_elem+r_num_elem)*elem_width);
-    free(temp_holder);
+    memcpy(merged, l_iter, l_end - l_iter);
+    merged += l_end - l_iter;
+    memcpy(merged, r_iter, r_end - r_iter);
+    merged += r_end - r_iter;
+    merged -= (l_num_elem + r_num_elem) * elem_width;
+    memcpy(to_merge, merged, (l_num_elem+r_num_elem)*elem_width);
+    free(merged);
     return inversions;
 }
