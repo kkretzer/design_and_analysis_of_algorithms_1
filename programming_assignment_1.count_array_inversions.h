cdb4c618633f73b87b84dbcba88fd0055169a397
Kurt Kretzer <kurt.kretzer@gmail.com>
2012-08-23 07:27:44 -0500

    refactoring mergesort a bit for clarity

--- programming_assignment_1.count_array_inversions.h
+++ programming_assignment_1.count_array_inversions.h
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define merge_elem(ary,ptr,num) memcpy(ary, ptr, num); \
+                                ary += num; \
+                                ptr += num;
+
 static size_t merge(void *, size_t, size_t, size_t, int (*compar)(const void *, const void *));
 
 size_t mymergesort(void *input_array, size_t num_elem, size_t elem_width, int (*compar)(const void *, const void *))
@@ -21,40 +25,30 @@ size_t mymergesort(void *input_array, size_t num_elem, size_t elem_width, int (*
 static size_t merge(void *to_merge, size_t l_num_elem, size_t r_num_elem, size_t elem_width, int (*compar)(const void *, const void *))
 {
     char *l_iter, *r_iter, *l_end, *r_end, *merged;
-    size_t inversions;
+    size_t inversions = 0, total_bytes = (l_num_elem + r_num_elem) * elem_width;
     int compar_result;
+
     l_iter = (char *)to_merge;
-    r_iter = l_end = l_iter + (l_num_elem * elem_width);
-    r_end = l_iter + (l_num_elem + r_num_elem)*elem_width;
-    inversions = 0;
-    merged = (char *) malloc((l_num_elem + r_num_elem) * elem_width);
+    r_iter = l_end = l_iter + l_num_elem * elem_width;
+    r_end = r_iter + r_num_elem * elem_width;
+    merged = (char *) malloc(total_bytes);
 
     while (l_iter < l_end && r_iter < r_end) {
         compar_result = (*compar)(l_iter, r_iter);
         if (compar_result < 0) {
-            memcpy(merged, l_iter, elem_width);
-            merged += elem_width;
-            l_iter += elem_width;
+            merge_elem(merged, l_iter, elem_width);
         } else if (compar_result > 0) {
-            memcpy(merged, r_iter, elem_width);
-            merged += elem_width;
-            r_iter += elem_width;
+            merge_elem(merged, r_iter, elem_width);
             inversions += ((l_end - l_iter)/elem_width);
         } else {
-            memcpy(merged, l_iter, elem_width);
-            merged += elem_width;
-            l_iter += elem_width;
-            memcpy(merged, r_iter, elem_width);
-            merged += elem_width;
-            r_iter += elem_width;
+            merge_elem(merged, l_iter, elem_width);
+            merge_elem(merged, r_iter, elem_width);
         }
     }
-    memcpy(merged, l_iter, l_end - l_iter);
-    merged += l_end - l_iter;
-    memcpy(merged, r_iter, r_end - r_iter);
-    merged += r_end - r_iter;
-    merged -= (l_num_elem + r_num_elem) * elem_width;
-    memcpy(to_merge, merged, (l_num_elem+r_num_elem)*elem_width);
+    merge_elem(merged, l_iter, l_end - l_iter);
+    merge_elem(merged, r_iter, r_end - r_iter);
+    merged -= total_bytes;
+    memcpy(to_merge, merged, total_bytes);
     free(merged);
     return inversions;
 }
